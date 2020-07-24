e31ab9c5925fd948000f0843a8401dbb1b2792eb
Felipe Vieira <flpvieira86@gmail.com>
2020-07-23 21:20:39 -0300

    api ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 export async function getCategories() {
-  // implement here
+  const apiUrl = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const fetchResult = fetch(apiUrl).then((response) => response.json());
+  return fetchResult;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+  let apiUrl;
+  if (categoryId) apiUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  else if (query) apiUrl = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  else apiUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  const fetchResult = fetch(apiUrl).then((response) => response.json());
+  return fetchResult;
 }
