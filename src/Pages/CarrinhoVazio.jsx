e36e4fa87211deb5ae21333be8045dd2835011fc
Felipe Vieira <flpvieira86@gmail.com>
2020-07-24 17:18:57 -0300

    CC fixes

--- src/Pages/CarrinhoVazio.jsx
+++ src/Pages/CarrinhoVazio.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 // import Imagem from './shopping-cart.jpg';
 
 class CarrinhoVazio extends React.Component {
-    render() {
-        return (
-            <div data-testid="shopping-cart-empty-message">Seu carrinho está vazio</div>
-        );
-    }
+  render() {
+    return <div data-testid="shopping-cart-empty-message">Seu carrinho está vazio</div>;
+  }
 }
 
 export default CarrinhoVazio;
--- src/Pages/ShoppingCart.jsx
+++ src/Pages/ShoppingCart.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import imagem from './shopping-cart.jpg';
 import CarrinhoVazinho from './CarrinhoVazio';
+// import imagem from './shopping-cart.jpg';
 
 class ShoppingCart extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = { products: [] };
-    }
-    
-    render() {
-      const { products } = this.state;
-      if (products.length === 0) return <CarrinhoVazinho />;
-        return (
-          <div>Carrinho de Compras</div>
-    );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  render() {
+    const { products } = this.state;
+    if (products.length === 0) return <CarrinhoVazinho />;
+    return <div>Carrinho de Compras</div>;
+  }
 }
 
 export default ShoppingCart;
