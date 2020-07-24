b225b9454e402827668b9d193497214bb73740a4
Thiago <asm.thiago@gmail.com>
2020-07-24 16:28:58 -0300

    Requisito 02 OK

---
+++ src/ProductList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class ProductList extends React.Component {
+  render(){
+    return(
+      <div data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </div>
+    )
+  }
+}
+
+export default ProductList;
