0fa5b532120ff788bb9bb1d1251c9e6419440a71
Felipe Vieira <flpvieira86@gmail.com>
2020-07-25 12:10:11 -0300

    adicionado placeholder no input

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,7 +4,7 @@ class ProductList extends React.Component {
   render() {
     return (
       <div className="product-list">
-        <div data-testid="home-initial-message">
+        <div className="hidden" data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </div>
       </div>
--- src/pages/SearchBar.jsx
+++ src/pages/SearchBar.jsx
@@ -5,7 +5,12 @@ export default class SearchBar extends Component {
   render() {
     return (
       <div className="search-bar">
-        <input className="search-input" data-testid="query-input" type="text" />
+        <input
+          placeholder="Digite algum termo de pesquisa ou escolha uma categoria"
+          className="search-input"
+          data-testid="query-input"
+          type="text"
+        />
         <img
           className="search-icon"
           src={searchIcon}
