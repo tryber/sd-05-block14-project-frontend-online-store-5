060cb8d7492c6717b53fd968485d50ef9c32cecd
Felipe Vieira <flpvieira86@gmail.com>
2020-07-28 17:42:30 -0300

    css

--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 class ProductInfo extends React.Component {
   render() {
-    const { produto } = this.props;
+    const { produto, produto: { thumbnail } } = this.props;
     return (
-      <div data-testid="product">
-        <Link to={`/details/${produto.id}`} className="product" data-testid="product-detail-link">
-          <img className="productImg" src={produto.thumbnail} alt={produto.title} />
-          <div>{produto.title}</div>
+      <div data-testid="product" className="product">
+        <Link to={`/details/${produto.id}`} className="prodLink" data-testid="product-detail-link">
+          <div className="productImgContainer">
+            <img className="productImg" src={thumbnail.replace(/-I/i, '-O')} alt={produto.title} />
+          </div>
+          <div className="productTitle">{produto.title}</div>
           <div className="productPrice">{`R$${produto.price.toFixed(2)}`}</div>
           {produto.shipping.free_shipping
             ? <div className="free-shipping" data-testid="free-shipping">Entrega grátis</div>
