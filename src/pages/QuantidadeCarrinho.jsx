2c1c9b283d9036254c326943e53122bf99a6a5fc
Felipe Vieira <flpvieira86@gmail.com>
2020-07-28 21:28:39 -0300

    Requisito 10 ok

---
+++ src/pages/QuantidadeCarrinho.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+export default class CartItemQnt extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { qnt: 1 };
+  }
+
+  increase() {
+    const { qnt } = this.state;
+
+    this.setState({ qnt: qnt + 1 });
+  }
+
+  decrease() {
+    const { qnt } = this.state;
+    if (qnt > 0) this.setState({ qnt: qnt - 1 });
+  }
+
+  render() {
+    const { qnt } = this.state;
+    const { inc, dec } = this.props;
+    return (
+      <div>
+        <button
+          onClick={() => { this.decrease(); dec(); }}
+          type="button"
+          data-testid="product-decrease-quantity"
+        >
+          -
+        </button>
+        <div data-testid="shopping-cart-product-quantity">{qnt}</div>
+        <button
+          onClick={() => { this.increase(); inc(); }}
+          type="button"
+          data-testid="product-increase-quantity"
+        >
+          +
+        </button>
+      </div>
+    );
+  }
+}
