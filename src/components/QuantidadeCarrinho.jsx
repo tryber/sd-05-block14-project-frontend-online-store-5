df199dbc052eed21aa4e17b58a3cbcd04408eb13
Felipe Vieira <flpvieira86@gmail.com>
2020-07-30 17:50:34 -0300

    la vai outra bomba

--- src/components/QuantidadeCarrinho.jsx
+++ src/components/QuantidadeCarrinho.jsx
@@ -7,45 +7,53 @@ export default class CartItemQnt extends Component {
     this.state = { qnt: qnt || 1 };
   }
 
-  increase() {
+  async increase() {
     const { qnt } = this.state;
-    const { max, inc, produto } = this.props;
+    const { max, inc, produto, getTotal } = this.props;
     if (qnt < max) {
-      this.setState({ qnt: Number(qnt) + 1 });
+      await this.setState({ qnt: Number(qnt) + 1 });
       inc(produto);
+      getTotal(qnt, 1);
     }
   }
 
-  decrease() {
-    const { dec, produto } = this.props;
+  async decrease() {
+    const { dec, produto, getTotal } = this.props;
     const { qnt } = this.state;
     if (qnt > 0) {
-      this.setState({ qnt: Number(qnt) - 1 });
+      await this.setState({ qnt: Number(qnt) - 1 });
       dec(produto);
+      getTotal(qnt, -1);
     }
   }
 
   render() {
+    const { produto: { product } } = this.props;
     const { qnt } = this.state;
     return (
-      <div className="cartQuantity">
-        <button
-          className="buttonQty"
-          onClick={() => this.decrease()}
-          type="button"
-          data-testid="product-decrease-quantity"
-        >
-          -
-        </button>
-        <div data-testid="shopping-cart-product-quantity">{qnt}</div>
-        <button
-          className="buttonQty"
-          onClick={() => this.increase()}
-          type="button"
-          data-testid="product-increase-quantity"
-        >
-          +
-        </button>
+      <div className="quantityContainer">
+        <div className="cartQuantity">
+          <button
+            className="buttonQty"
+            onClick={() => { this.decrease(); }}
+            type="button"
+            data-testid="product-decrease-quantity"
+          >
+            -
+          </button>
+          <div data-testid="shopping-cart-product-quantity">{qnt}</div>
+          <button
+            className="buttonQty"
+            onClick={() => this.increase()}
+            type="button"
+            data-testid="product-increase-quantity"
+          >
+            +
+          </button>
+        </div>
+        <div className="shoppingCartPrice">
+          {`R$${(product.price * qnt).toFixed(2)}`}
+        </div>
       </div>
     );
   }
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -7,12 +7,27 @@ class ShoppingCart extends React.Component {
   constructor(props) {
     super(props);
     const { cart } = this.props;
-    this.state = { products: cart };
+    this.state = { products: cart, total: 0 };
     this.loadCart = this.loadCart.bind(this);
+    this.getTotal = this.getTotal.bind(this);
   }
 
   componentDidMount() {
     this.loadCart();
+  }
+
+  componentDidUpdate(prevProp, prevState) {
+    const { products } = this.state;
+    if (prevState.products !== products) this.getTotal();
+  }
+
+  getTotal(qnt, gambiarra) {
+    const { products } = this.state;
+    console.log(products);
+    const total = products
+      .reduce((acc, produto) => (acc + (produto.product.price
+        * (qnt + gambiarra || produto.quantity))), 0);
+    this.setState({ total: total.toFixed(2) });
   }
 
   loadCart() {
@@ -22,21 +37,31 @@ class ShoppingCart extends React.Component {
 
   render() {
     const { inc, dec } = this.props;
-    const { products } = this.state;
+    const { products, total } = this.state;
     if (products.length === 0) return <CarrinhoVazinho />;
     return (
       <div className="shoppingCart">
         <Link to="/">Voltar</Link>
-        {products ? products.map((product) => (
-          <div className="shoppingCartContainer" key={product.product.id}>
-            <div data-testid="shopping-cart-product-name">{product.product.title}</div>
-            <QuantidadeCarrinho
-              max={product.product.available_quantity}
-              inc={inc} dec={dec} qnt={product.quantity} produto={product}
-            />
-          </div>
-        )) : null}
-        <Link to="/checkout" data-testid="checkout-products">Finalizar Compra</Link>
+        {products
+          ? products.map((product) => (
+            <div className="shoppingCartContainer" key={product.product.id}>
+              <img src={product.product.thumbnail} alt="product thumbnail" />
+              <div className="shoppingCartTitle" data-testid="shopping-cart-product-name">
+                {product.product.title}
+              </div>
+              <QuantidadeCarrinho
+                max={product.product.available_quantity} inc={inc} dec={dec}
+                qnt={product.quantity} produto={product} getTotal={this.getTotal}
+              />
+            </div>
+          ))
+          : null}
+        {products.length > 0 ? <div className="total">{`Total: R$${total}`}</div> : null}
+        <div className="cartBtnContainer">
+          <Link className="FinalizarCompra" to="/checkout" data-testid="checkout-products">
+            Finalizar Compra
+          </Link>
+        </div>
       </div>
     );
   }
